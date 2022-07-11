eb9a2a853ff12b6f8f7dd80d555766593e2913b8
messif <Messifsalaheddine1@gmail.com>
2022-07-11 07:13:49 -0700

    complete version

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const menuBotton = document.querySelector(".fa-bars");
-const navigation = document.querySelector(".nav-links");
+const navigation = document.querySelector(".link-container");
+const close = document.querySelector(".icon-close");
 
 menuBotton.addEventListener("click", function () {
   //ADDED CLASSES FOR NAVIGATION
   navigation.style.transform = "translateX(0)";
   console.log("clicked");
+});
+close.addEventListener("click", function () {
+  navigation.style.transform = "translateX(-100vh)";
 });
